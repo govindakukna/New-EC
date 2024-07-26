99dceb7bfec25321dbf247d215438847d03fa986
govindakukna <kuknagovinda@gmail.com>
2024-07-27 00:59:38 +0530

    first commit

---
+++ frontend/src/pages/StripeCheckout.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { useSelector } from 'react-redux';
+
+import CheckoutForm from "./CheckoutForm";
+import "../Stripe.css";
+import { selectCurrentOrder } from "../features/order/orderSlice";
+
+
+const stripePromise = loadStripe(
+  "pk_test_51Pck95D0oV2Ou1caUBI0N7gEntjHbyqvvGWHnH8PbSiVEvMTY2iCGQuvJevKN3zyk32wcTaeRq9qgrwqXmkRySiF00t3kllC8q"
+);
+
+export default function StripeCheckout() {
+  const [clientSecret, setClientSecret] = useState("");
+  const currentOrder = useSelector(selectCurrentOrder)
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ totalAmount: currentOrder.totalAmount, orderId:currentOrder.id }),
+    
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: 'stripe',
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div className="Stripe">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
+}
